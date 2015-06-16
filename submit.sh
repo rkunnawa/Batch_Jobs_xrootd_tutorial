c977f071f0341e2e332db3859e3c14a8e0397f11
Raghav Kunnawalkam Elayavalli <raghav.k.e@cern.ch>
2015-06-16 17:03:05 +0200

    update to submit.sh

--- submit.sh
+++ submit.sh
@@ -6,19 +6,20 @@ eval `scramv1 runtime -sh`
 
 cd /afs/cern.ch/work/r/rkunnawa/BatchJobsTutorial/CMSSW_5_3_20/src
 
+echo "root -l -b -q macro.C++"
+echo "First = $FIRST and last file = $LAST"   
 
-
-  echo "root -l -b -q merge_kurt_files_V2.C++"
-  echo "First = $FIRST and last file = $LAST"   
-
-root -b > merge_kurt_files${FIRST}.log <<EOF
-.x merge_kurt_files_V2.C($FIRST,$LAST);
+root -b > macro${FIRST}.log <<EOF
+.x macro.C($FIRST,$LAST);
 .q
 EOF
 
 
-
 echo "Done all jobs!"
 
-#echo "Copying output files to " $destination
+destination="/path/to/output/file/"
+
+echo "Copying output files to " $destination
+
+mv test_outputFile_$LAST.root $destination 
 
